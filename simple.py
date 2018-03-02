45c18e03881ec7fb98aa64f9982f499b48eace8c
Antonio Monteagudo Chaves <themonti@gmail.com>
2018-03-02 01:00:15 +0100

    v1

---
+++ simple.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template
+from datetime import datetime
+import engineio
+
+# set async_mode to 'threading', 'eventlet' or 'gevent' to force a mode
+# else, the best mode is selected automatically from what's installed
+async_mode = None
+
+eio = engineio.Server(async_mode=async_mode)
+app = Flask(__name__)
+app.wsgi_app = engineio.Middleware(eio, app.wsgi_app)
+
+
+@app.route('/')
+def index():
+    return render_template('simple.html')
+
+
+@eio.on('connect')
+def connect(sid, environ):
+    print("connect ", sid)
+
+
+@eio.on('message')
+def message(sid, data):
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+    print('{time}message from'.format(time=the_time), sid, data)
+    eio.send(sid, '{0} mensaje recibido por {1}: {2}'.format(the_time,sid,data), binary=False)
+
+
+
+@eio.on('disconnect')
+def disconnect(sid):
+    print('disconnect ', sid)
+
+
+if __name__ == '__main__':
+    if eio.async_mode == 'threading':
+        # deploy with Werkzeug
+        app.run(threaded=True)
+    elif eio.async_mode == 'eventlet':
+        # deploy with eventlet
+        import eventlet
+        from eventlet import wsgi
+        wsgi.server(eventlet.listen(('', 5000)), app)
+    elif eio.async_mode == 'gevent':
+        # deploy with gevent
+        from gevent import pywsgi
+        try:
+            from geventwebsocket.handler import WebSocketHandler
+            websocket = True
+        except ImportError:
+            websocket = False
+        if websocket:
+            pywsgi.WSGIServer(('', 5000), app,
+                              handler_class=WebSocketHandler).serve_forever()
+        else:
+            pywsgi.WSGIServer(('', 5000), app).serve_forever()
+    else:
+        print('Unknown async_mode: ' + eio.async_mode)
